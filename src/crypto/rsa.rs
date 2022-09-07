10701d143341c1d73f61fb7b7df233e44a3ea9d2
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-09-07 18:31:05 +0000

    Use `ring` 0.17 API

--- src/crypto/rsa.rs
+++ src/crypto/rsa.rs
@@ -39,7 +39,7 @@ pub(crate) fn sign(
     message: &[u8],
 ) -> Result<String> {
     let key_pair = signature::RsaKeyPair::from_der(key)
-        .map_err(|e| ErrorKind::InvalidRsaKey(e.description_()))?;
+        .map_err(|e| ErrorKind::InvalidRsaKey(e.to_string()))?;
 
     let mut signature = vec![0; key_pair.public_modulus_len()];
     let rng = rand::SystemRandom::new();
